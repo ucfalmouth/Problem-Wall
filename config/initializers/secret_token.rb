258dc73f946af69cba10815999326a43ffebe0af
Damien Hogan <damienhogan87@gmail.com>
2014-04-26 13:40:15 +0100

    initial first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PW::Application.config.secret_key_base = '3c7062bc55d9f3621592d3bbcd087cafd9587582be332eb258ef664d90531019e48ed229e2a569bad53c322e39587fe864341d005740b7e0985f5300b62ec01a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PW::Application.config.session_store :cookie_store, key: '_PW_session'
